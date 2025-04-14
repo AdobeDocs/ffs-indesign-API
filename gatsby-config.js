d208f22884d05b8fb304ea8bb1febf75198aa70f
Diane Batres <dianeba3@gmail.com>
2025-04-14 16:00:04 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@
  */
 
 module.exports = {
-  pathPrefix: process.env.PATH_PREFIX || '/firefly-services/docs/photoshop/',
+  pathPrefix: process.env.PATH_PREFIX || '/firefly-services/docs/indesign-apis/',
   siteMetadata: {
     versions: [
       {
